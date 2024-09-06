96e5fe8efccc7cbcf895f79423511979e76e8e6a
ashokkumar2021 <ashokmalhan001@gmail.com>
2024-09-06 22:53:05 +0530

    comment

--- components/header.js
+++ components/header.js
@@ -4,13 +4,11 @@ import Link from 'next/link'
 //header function
 
 function Header() {
-
-
     return (
         <div className={styles.main}>
             <Link href={'/'}><a>Home</a></Link>
             <Link href={'/feed/1'}><a>Feed</a></Link>
-            <Link href={'/about'}><a>About Me</a></Link>
+            <Link href={'/about'}><a>About Me</a></Link> //About us page
         </div>
     )
 }
